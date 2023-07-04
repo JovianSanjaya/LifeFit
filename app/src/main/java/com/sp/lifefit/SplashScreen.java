0fa4b4b2e63939634025a54dc130c6dee905c44e
Oddkaboomtnt <vnobile0629@gmail.com>
2023-07-04 13:54:27 +0800

    ho

--- app/src/main/java/com/sp/lifefit/SplashScreen.java
+++ app/src/main/java/com/sp/lifefit/SplashScreen.java
@@ -1,4 +1,25 @@
 package com.sp.lifefit;
 
-public class SplashScreen {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+    private static final int SPLASH_DELAY=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.care_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(CareSplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
 }
