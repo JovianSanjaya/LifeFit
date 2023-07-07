57d410ce943d17a29f8f312c38ace06861fb4d61
xkvnxx <132749552+xkvnxx@users.noreply.github.com>
2023-07-07 18:58:56 +0800

    today's update

---
+++ app/src/main/java/com/sp/lifefit/Onboarding/CaregiverVSCarerecipient.java
@@ -0,0 +1,18 @@
+package com.sp.lifefit.Onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sp.lifefit.R;
+
+public class CaregiverVSCarerecipient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.caregivervscarerecipient);
+
+
+    }
+}
--- app/src/main/java/com/sp/lifefit/Onboarding/SplashScreen.java
+++ app/src/main/java/com/sp/lifefit/Onboarding/SplashScreen.java
@@ -1,25 +1,24 @@
-package com.sp.lifefit;
+package com.sp.lifefit.Onboarding;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Button;
+import com.sp.lifefit.R;
 
 public class SplashScreen extends AppCompatActivity {
     private static final int SPLASH_DELAY = 3000;
     private Button button;
-    //hello
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.care_splash_screen);
+        setContentView(R.layout.splash_screen);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(CareSplashScreen.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, Activity_OnBoarding1.class);
                 startActivity(intent);
                 finish();
             }
