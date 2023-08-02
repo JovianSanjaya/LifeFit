192c32b5b2958209a022cfb5537a77dc92c4bf26
Riceandcurry <Ricefactory237@gmail.com>
2023-08-02 10:40:15 +0800

    this

--- app/src/main/java/com/sp/lifefit/Onboarding/CareRecipient/HealthProfileCareRecipient.java
+++ app/src/main/java/com/sp/lifefit/Onboarding/CareRecipient/HealthProfileCareRecipient.java
@@ -6,12 +6,13 @@ import androidx.appcompat.widget.AppCompatButton;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.sp.lifefit.R;
 
 public class HealthProfileCareRecipient extends AppCompatActivity {
 
-    private AppCompatButton nextbtn;
+    private Button nextbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
