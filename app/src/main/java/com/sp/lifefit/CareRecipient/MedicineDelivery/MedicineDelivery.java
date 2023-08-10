2962394e2ed52a1e9aae9a10e13e7c8ff63ed9ac
Riceandcurry <Ricefactory237@gmail.com>
2023-08-10 12:10:28 +0800

    new wut

--- app/src/main/java/com/sp/lifefit/CareRecipient/MedicineDelivery/MedicineDelivery.java
+++ app/src/main/java/com/sp/lifefit/CareRecipient/MedicineDelivery/MedicineDelivery.java
@@ -7,11 +7,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.sp.lifefit.CareRecipient.MainCareRecipient;
 import com.sp.lifefit.R;
 
 public class MedicineDelivery extends AppCompatActivity {
     private AppCompatButton requestbtn;
+    private ImageButton backbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -19,10 +22,21 @@ public class MedicineDelivery extends AppCompatActivity {
         setContentView(R.layout.medicine_delivery);
 
         requestbtn = findViewById(R.id.requestbtn_medicinedelivery);
+        backbtn = findViewById(R.id.backbtn3);
+
         requestbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MedicineDelivery.this, MedicinePharmacy.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MedicineDelivery.this, MainCareRecipient.class);
                 startActivity(intent);
                 finish();
             }
