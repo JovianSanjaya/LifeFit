ef4820e3d65a45a9fd24924d083a057bad8117ec
Riceandcurry <Ricefactory237@gmail.com>
2023-08-11 12:34:45 +0800

    new wut

--- app/src/main/java/com/sp/lifefit/CareRecipient/TeleConsultation/TeleConsultation.java
+++ app/src/main/java/com/sp/lifefit/CareRecipient/TeleConsultation/TeleConsultation.java
@@ -6,13 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.sp.lifefit.CareRecipient.HomeDoctor.HomeDoctor;
+import com.sp.lifefit.CareRecipient.MainCareRecipient;
 import com.sp.lifefit.R;
 
 public class TeleConsultation extends AppCompatActivity {
 
     private Button requestnowbtn;
+    private ImageButton backbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -20,6 +23,7 @@ public class TeleConsultation extends AppCompatActivity {
         setContentView(R.layout.tele_consultation_request);
 
         requestnowbtn = findViewById(R.id.requestnow_teleconsulatation);
+        backbtn = findViewById(R.id.tele_consultation_back_button);
         requestnowbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,6 +34,18 @@ public class TeleConsultation extends AppCompatActivity {
 
             }
         });
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(TeleConsultation.this, MainCareRecipient.class );
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
 
     }
 }
