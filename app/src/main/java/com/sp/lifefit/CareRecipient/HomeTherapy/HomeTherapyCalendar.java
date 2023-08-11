f3918ee8e5b933fc885d0a94006603bc836ca9e7
Riceandcurry <Ricefactory237@gmail.com>
2023-08-11 12:28:15 +0800

    new wut

--- app/src/main/java/com/sp/lifefit/CareRecipient/HomeTherapy/HomeTherapyCalendar.java
+++ app/src/main/java/com/sp/lifefit/CareRecipient/HomeTherapy/HomeTherapyCalendar.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.sp.lifefit.Payment.Payment;
 import com.sp.lifefit.R;
@@ -13,6 +14,7 @@ import com.sp.lifefit.R;
 public class HomeTherapyCalendar extends AppCompatActivity {
 
     private Button confirmbtn;
+    private ImageButton backbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +22,20 @@ public class HomeTherapyCalendar extends AppCompatActivity {
         setContentView(R.layout.hometherapy_calender);
 
         confirmbtn = findViewById(R.id.confirm_order_btn);
+        backbtn = findViewById(R.id.home_therapy_back_button);
         confirmbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomeTherapyCalendar.this, Payment.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeTherapyCalendar.this, HomeTherapy.class);
                 startActivity(intent);
                 finish();
             }
