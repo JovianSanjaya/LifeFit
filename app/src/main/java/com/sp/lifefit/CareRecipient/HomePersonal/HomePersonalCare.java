c9e7a8485d87694dca4e3eb25ccf23ff32e50b56
Riceandcurry <Ricefactory237@gmail.com>
2023-08-11 12:14:15 +0800

    new wut

--- app/src/main/java/com/sp/lifefit/CareRecipient/HomePersonal/HomePersonalCare.java
+++ app/src/main/java/com/sp/lifefit/CareRecipient/HomePersonal/HomePersonalCare.java
@@ -6,13 +6,16 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
 import com.sp.lifefit.CareRecipient.HomePersonal.Housekeeping.Housekeeping;
 import com.sp.lifefit.CareRecipient.HomePersonal.MealArrangement.MealMenuList;
+import com.sp.lifefit.CareRecipient.MainCareRecipient;
 import com.sp.lifefit.R;
 
 public class HomePersonalCare extends AppCompatActivity {
     private CardView meal,housekeeping;
+    private ImageButton backbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -20,6 +23,7 @@ public class HomePersonalCare extends AppCompatActivity {
 
         meal = findViewById(R.id.meal_arrangement);
         housekeeping = findViewById(R.id.housekeeping);
+        backbtn = findViewById(R.id.backbtn5);
 
         meal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +42,16 @@ public class HomePersonalCare extends AppCompatActivity {
                 finish();
             }
         });
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePersonalCare.this, MainCareRecipient.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/sp/lifefit/CareRecipient/HomeTherapy/HomeTherapy.java
+++ app/src/main/java/com/sp/lifefit/CareRecipient/HomeTherapy/HomeTherapy.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.sp.lifefit.CareRecipient.MainCareRecipient;
 import com.sp.lifefit.R;
 
 public class HomeTherapy extends AppCompatActivity {
 
     private Button requestbtn;
+    private ImageButton backbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +22,20 @@ public class HomeTherapy extends AppCompatActivity {
         setContentView(R.layout.hometherapy_request);
 
         requestbtn = findViewById(R.id.requestnow_hometherapy);
+        backbtn = findViewById(R.id.backbtn2);
         requestbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomeTherapy.this, HomeTherapyCalendar.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeTherapy.this, MainCareRecipient.class);
                 startActivity(intent);
                 finish();
             }
