159a8b00efd6abeaff15b0a7332368332211ec1e
Riceandcurry <Ricefactory237@gmail.com>
2023-08-11 15:21:47 +0800

    new wut

--- app/src/main/java/com/sp/lifefit/Caregiver/HomeFragmentCareGiver.java
+++ app/src/main/java/com/sp/lifefit/Caregiver/HomeFragmentCareGiver.java
@@ -32,24 +32,8 @@ public class HomeFragmentCareGiver extends Fragment {
 
         getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        meal = rootView.findViewById(R.id.meal_arrangement_onactivity_caregiver);
-        homedoctor = rootView.findViewById(R.id.home_doctor_list_onactivity_caregiver);
 
-        meal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), MealArrangementListOnActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        homedoctor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), HomeDoctorListOnActivity.class);
-                startActivity(intent);
-            }
-        });
+        
 
 
 
